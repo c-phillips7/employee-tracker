25be7863e7f008290fbc047b66326995f53b70a5
Chris Phillips <cp.phillips15@gmail.com>
2021-12-26 16:54:56 -0500

    added connection to server using documentation of dotenv

--- app.js
+++ app.js
@@ -3,3 +3,9 @@ const mysql2 = require("mysql2");
 const inquirer = require("inquirer");
 const consoleTable = require('console.table');
 
+var connection = mysql2.createConnection({
+    host: "localhost",
+    port: 3001,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS
+});
