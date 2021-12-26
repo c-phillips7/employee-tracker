6c99e4f96cf2a59ce7e5a6f678116d9378c6d42e
Chris Phillips <cp.phillips15@gmail.com>
2021-12-26 18:30:11 -0500

    added back results = dotenv.config() as server would not read password without it, also added test of figlet for start up display

--- app.js
+++ app.js
@@ -4,18 +4,32 @@ const inquirer = require("inquirer");
 const consoleTable = require('console.table');
 const figlet = require('figlet');
 
+// Set env so it can be accessed
+const result = dotenv.config()
+if (result.error) {
+  throw result.error
+}
+
 // Create connection with express and mysql2
 var connection = mysql2.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: process.env.DB_PASS,
+    // password: "Waterisgood1!",
     database: "employee_db"
 });
 
 // run js loagic on connection
 connection.connect(function (err) {
     if (err) throw err;
+    figlet('Employee Tracker', function (err, data) {
+        if (err) {
+          console.log('Something went wrong...');
+          return;
+        };
+        console.log(data);
+    });
     console.log("connection working");
     init();
 });
