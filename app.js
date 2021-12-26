3453a254061ee529544071fe29bc1f8253d8542c
Chris Phillips <cp.phillips15@gmail.com>
2021-12-26 18:20:55 -0500

    fixed connection to mysql2 and set up conect function to test server is running and inquirer starts

--- app.js
+++ app.js
@@ -4,9 +4,63 @@ const inquirer = require("inquirer");
 const consoleTable = require('console.table');
 const figlet = require('figlet');
 
+// Check if .env loading correctly
+const result = dotenv.config()
+if (result.error) {
+  throw result.error
+}
+console.log(result.parsed)
+
+// Create connection with express and mysql2
 var connection = mysql2.createConnection({
     host: "localhost",
-    port: 3001,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS
+    port: 3306,
+    user: "root",
+    password: process.env.DB_PASS,
+    database: "employee_db"
 });
+
+// run js loagic on connection
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connection working");
+    init();
+});
+
+// Used to debug error connecting to server
+process.on('uncaughtException', function (err) {
+    console.log(err);
+}); 
+
+// basic function to test server
+function init() {
+    inquirer
+        .prompt(test)
+        .then(console.log("inquirer ran"))
+        .catch(err => {
+            console.log("Error: ", err)
+        })
+};
+
+// basic question to test inquirer
+const test = [{
+        type: "input",
+        message: "Is this working: ",
+        name: "test",
+    }];
+
+//TODO: add View all departments
+
+// TODO: add View all Roles
+
+// TODO: add View all Employees
+
+
+
+// TODO: add Add new department
+
+// TODO: add Add new role
+
+// TODO: add Add new employee
+
+// TODO: add Update an employee
